8b637c453f4501687cc69dc7075b7128bd1c74e4
dk731 <demid.kaidalov@gmail.com>
2021-08-10 14:14:33 +0300

    moved setup.py to the root

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extra_link = [
 extra_macros = [("VIRT_CUBE", None)]
 led_module = Extension(
     "_ledcd",
-    sources=["swig_module_wrap.cxx", "CubeDrawer.cpp"],
+    sources=["src/swig_module_wrap.cxx", "src/CubeDrawer.cpp"],
     extra_link_args=extra_link,
     define_macros=extra_macros,
     # extra_objects=["/usr/local/lib64/libws.a"]
