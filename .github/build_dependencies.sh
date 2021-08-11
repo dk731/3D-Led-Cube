9f5242631cd515417dbe980dd519721f83003475
dk731 <demid.kaidalov@gmail.com>
2021-08-11 13:40:59 +0300

    Debugging GLEW build

--- .github/build_dependencies.sh
+++ .github/build_dependencies.sh
@@ -18,7 +18,9 @@ cd ${root_dir}
 
 echo "Building GLEW..."
 wget https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.tgz
+echo "Downloaded glew.tgz"
 tar -xf glew-2.2.0.tgz
+echo "Unpacked glew.tgz"
 cd glew-2.2.0
 make glew.lib
 mv ./include/* ${root_dir}/include
