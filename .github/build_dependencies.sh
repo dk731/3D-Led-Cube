8c811e9d191fe5154f10a0138a09b0d9e6f8ad38
dk731 <demid.kaidalov@gmail.com>
2021-08-11 14:11:33 +0300

    Revert "Debugging GLEW build"
    This reverts commit 9f5242631cd515417dbe980dd519721f83003475.

---
+++ .github/build_dependencies.sh
@@ -0,0 +1,27 @@
+echo "Prepering folder structure..."
+root_dir=$(pwd)
+mkdir include
+mkdir lib
+
+echo "Building CBLAS..."
+git clone --depth 1 https://github.com/Reference-LAPACK/lapack-release
+cd lapack-release
+mv make.inc.example make.inc
+mkdir build
+cd build
+cmake -DCBLAS=ON -DBUILD_SHARED_LIBS=ON ..
+make cblas
+
+mv ./lib/* ${root_dir}/lib
+mv ./include/* ${root_dir}/include
+cd ${root_dir}
+
+echo "Building GLEW..."
+wget https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.tgz
+tar -xf glew-2.2.0.tgz
+cd glew-2.2.0
+make glew.lib
+mv ./include/* ${root_dir}/include
+mv ./lib/* ${root_dir}/lib
+
+cd ${root_dir}
