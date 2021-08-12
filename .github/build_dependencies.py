a9ed6cc0b06477cc3487651b1609a535d4c6e1f8
dk731 <demid.kaidalov@gmail.com>
2021-08-12 10:13:16 +0300

    Moved building script to python

---
+++ .github/build_dependencies.py
@@ -0,0 +1,87 @@
+import os
+import subprocess
+import glob
+import shutil
+import requests
+import tarfile
+
+root_dir = os.getcwd()
+include_dir = os.path.join(root_dir, "include")
+lib_dir = os.path.join(root_dir, "lib")
+
+print("Prepearing folder structure...")
+os.mkdir(os.path.join(root_dir, "lib"))
+
+
+def build_glfw():
+    os.chdir(root_dir)
+    print("Building GLFW...")
+    subprocess.call(
+        "git clone --depth 1 https://github.com/glfw/glfw",
+        shell=True,
+    )
+    os.mkdir("./glfw/build")
+    os.chdir("./glfw/build")
+    subprocess.call(
+        [
+            "cmake",
+            "-DBUILD_SHARED_LIBS=ON",
+            "-DGLFW_BUILD_EXAMPLES=OFF",
+            "-DGLFW_BUILD_TESTS=OFF",
+            "-DGLFW_BUILD_DOCS=OFF",
+            "-DGLFW_INSTALL=OFF",
+            "..",
+        ]
+    )
+
+    subprocess.call(["make"])
+
+    for file in glob.glob("../include/*"):
+        shutil.move(file, include_dir)
+    for file in glob.glob("./src/libglfw*"):
+        shutil.move(file, lib_dir)
+
+
+def build_cblas():
+    os.chdir(root_dir)
+    print("Building CBLAS...")
+    subprocess.call(
+        "git clone --depth 1 https://github.com/Reference-LAPACK/lapack-release",
+        shell=True,
+    )
+    os.rename("./lapack-release/make.inc.example", "./lapack-release/make.inc")
+    os.mkdir("./lapack-release/build")
+    os.chdir("./lapack-release/build")
+
+    subprocess.call(["cmake", "-DCBLAS=ON", "-DBUILD_SHARED_LIBS=ON", ".."])
+    subprocess.call(["make", "cblas"])
+
+    for file in glob.glob("./include/*"):
+        shutil.move(file, include_dir)
+    for file in glob.glob("./lib/*"):
+        shutil.move(file, lib_dir)
+
+
+def build_glew():
+    os.chdir(root_dir)
+    print("Building GLEW...")
+
+    response = requests.get(
+        "https://github.com/nigels-com/glew/releases/download/glew-2.2.0/glew-2.2.0.tgz",
+        stream=True,
+    )
+    file = tarfile.open(fileobj=response.raw, mode="r|gz")
+    file.extractall(path=".")
+
+    os.chdir("glew-2.2.0")
+    subprocess.call(["make", "glew.lib"])
+
+    for file in glob.glob("./include/GL/*"):
+        shutil.move(file, os.path.join(include_dir, "GL"))
+    for file in glob.glob("./lib/*"):
+        shutil.move(file, lib_dir)
+
+
+build_cblas()
+build_glew()
+build_glfw()
